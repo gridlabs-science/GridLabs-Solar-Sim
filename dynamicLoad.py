6cd3cb604bba98802e571dc41006024539870dfa
keegreil <keegreil@gmail.com>
2024-03-05 17:10:33 -0500

    Update dynamicLoad.py

--- dynamicLoad.py
+++ dynamicLoad.py
@@ -6,47 +6,67 @@ class dynamicLoad:
     #curtailDelay = 1 #in seconds.  The delay between being given the command to switch to standby/low power mode, and the time power use is actually reduced.
 
 
-    def __init__(self, pwrScaleUpSpeed, pwrScaleDownSpeed, curtailDelay, maxPower, minPower, capacitorSize, initialPanelVoltage, initialCapVoltage):
+    def __init__(self, pwrScaleUpSpeed, pwrScaleDownSpeed, curtailDelay, bootDelay, maxPower, minPower, capacitorSize, initialPanelVoltage, targetDecrement):
         #system constants
         self.pwrScaleUpSpeed = pwrScaleUpSpeed
         self.pwrScaleDownSpeed = pwrScaleDownSpeed
         self.curtailDelay = curtailDelay
+        self.bootDelay = bootDelay
         self.maxPower = maxPower
         self.minPower = minPower
         self.capacitorSize = capacitorSize # the size of the capacitor in Farads
+        self.targetDecrement = targetDecrement
         #variables for initialization.  ALL of these should be updated each time "get_power()" is called
         self.target = maxPower
-        self.state = 'running' # also 'curtailed', 'curtailing' (given delay between command and power reduction)
+        self.state = 'running' # also 'curtailed', 'curtailing', and 'booting' (given delay between command and power reduction)
         self.curtailTime = 0 # the time since the "curtail" command was given.  Used for the curtail delay logic
         self.lastPanelVoltage = initialPanelVoltage
-        self.lastCapVoltage = initialCapVoltage
+        #self.lastCapVoltage = initialCapVoltage
         self.lastPower = self.target
+        
 
-    def get_power(self, panelVoltage, capacitorVoltage, dt, solarPanel):
+    def get_power(self, panelVoltage, dt, solarPanel):
         #First, calculate the amps to/from the cap based on the delta in capacitor voltage
         # Increasing voltage means capacitor is charging.  Positive values of 'capAmps' indicate panel power is greater than load power
-        capAmps = (capacitorVoltage - self.lastCapVoltage) / (self.capacitorSize * dt)
+        capAmps = (panelVoltage - self.lastPanelVoltage) / (self.capacitorSize * dt)
 
         #Next, calculate the average power coming from the panel
-        avgCapVolts = (capacitorVoltage + self.lastCapVoltage)/2
+        avgCapVolts = (panelVoltage + self.lastPanelVoltage)/2
         avgPanelPower = self.lastPower + capAmps * avgCapVolts
 
         #Calculate panel irradiance based on its power output and panel voltage
-        avgPanelVolts = (self.lastPanelVoltage + panelVoltage) / 2
-        avgPanelCurrent = avgPanelPower / avgPanelVolts
-        irr = solarPanel.get_irradiance(avgPanelCurrent, avgPanelVolts)  #returns an estimate for the panel irradiance.  Max is 1000 W/m^2
+        avgPanelCurrent = avgPanelPower / avgCapVolts
+        irr = solarPanel.get_irradiance(avgPanelCurrent, avgCapVolts)  #returns an estimate for the panel irradiance.  Max is 1000 W/m^2
         
         #Calculate the max power available, if panel was at MPP, given current irradiance:
         powerAvailable = solarPanel.panel_output(solarPanel.Vmp, irr)
 
-        #Update target power to equal the incoming panel power, adjusted for buffer decrement
-        self.target = powerAvailable * 0.75
+        #Update target power to equal the incoming panel power, adjusted for buffer decrement.  1.0 = MPPT.  < 1 gives a buffer to deal with reaction delays.
+        self.target = powerAvailable * self.targetDecrement
 
-        #if self.target < self.minPower:
-            #Curtail, because we can't run slow enough
+        # State machine logic
+        if self.state == 'curtailing':  # So we already began the process of shutting down
+            self.curtailTime = self.curtailTime + dt
+            if self.curtailTime >= self.curtailDelay:  #We've waited the requisite delay time, so chip is now in standby mode
+                self.curtailTime = 0
+                self.state = 'curtailed'
+                self.lastPower = 0
+        if self.state == 'booting':
+            self.curtailTime = self.curtailTime + dt
+            if self.curtailTime >= self.bootDelay:  #We've fully booted, so start the ASIC at min power
+                self.curtailTime = 0
+                self.state = 'running'
+                self.lastPower = self.minPower
+
+        if self.target < self.minPower: #Curtail, because we can't run slow enough
+            self.state = 'curtailing'
+            self.curtailTime = 0
+        if self.target > self.minPower:
+            self.target = self.target
 
 
-        return 15
+
+        return self.lastPower
 
 
 
